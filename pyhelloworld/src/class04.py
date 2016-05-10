2dd752eb1088c7856d268d1092ba8f814d614eda
ycliu912 <ycliu912@qq.com>
2016-05-10 23:17:48 +0800

    add class04

---
+++ pyhelloworld/src/class04.py
@@ -0,0 +1,19 @@
+#-*-coding:utf-8 -*-
+'''
+Created on 2016年5月10日
+
+@author: liu
+'''
+##方法重写
+
+class Parent:           #定义父类
+    def myMethod(self):
+        print "调用父类方法"
+
+
+class Child(Parent):    #定义子类
+    def myMethod(self):
+        print "调用子类方法"
+
+c = Child()             #子类实例
+c.myMethod()            #子类调用重写方法
