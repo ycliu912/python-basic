331ff4205d4358c8aefb6b3e10d69ef186a0e25c
ycliu912 <ycliu912@qq.com>
2016-05-24 17:51:31 +0800

    python-json-dict-list

--- python-jenkins/jenkins-get-build-info.py
+++ python-jenkins/jenkins-get-build-info.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#coding:utf-8
+
 #-*-coding:utf-8 -*-
 #http://python-jenkins.readthedocs.io/en/latest/examples.html
 
@@ -34,37 +37,41 @@ print "server.get_queue_info() = ",server.get_queue_info()
 version = server.get_version()
 print "version = ",version
 
-job_info_00_parent160518 = server.get_job_info('00_parent160518', 1)
-#print job_info_00_parent160518
-#json_dumps(job_info_00_parent160518)
-#f = json.load(job_info_00_parent160518).keys
-#print f
-#print f.keys()
+
+builds = server.get_running_builds()
+print "server.get_running_builds() = ",builds
+
+#build00 = builds01 = server.get_job_info('test-agency')['lastBuild']
+build00 = builds01 = server.get_job_info('test-agency')
+print "builds01 = server.get_job_info('test-agency')['lastBuild'] = ",build00
+#print json_dumps(build00)
+
+#获取job名为job_name的job的最后次构建号
+builds01 = server.get_job_info('test-agency')['lastBuild']['number']
+print "server.get_job_info('test-agency')['lastBuild']['number'] = ",builds01
+
+builds02 = server.get_build_info('test-agency',159)
+print "server.get_build_info() = ",builds02
+#print json_dumps(builds02)
+
+ #判断job名为job_name的job的某次构建是否还在构建中
+builds03 = server.get_build_info('test-agency',159)['building']
+print "server.get_job_info('test-agency')['building'] = ",builds03
+
+#判断job名为job_name的job的某次构建的执行结果状态
+builds04 = server.get_build_info('test-agency',159)['result']
+print "server.get_build_info('test-agency',159)['result'] = ",builds04
+
+#获取svn revision
+builds05 = server.get_build_info('test-agency',159)['changeSet']['revisions'][0]['revision']
+builds06 = server.get_build_info('test-agency',159)['changeSet']['revisions'][0]['module']
+print "server.get_build_info('test-agency',159)['changeSet']['revisions'] type = ",type(server.get_build_info('test-agency',159)['changeSet']['revisions'])
+print "sever.get_build_info('test-agency','lastBuild')['revisions']['revision'] = ",builds05
+print "server.get_build_info('test-agency',159)['changeSet']['revisions'][0]['module'] = ",builds06
+print "builds05.type = ",type(builds05)
+#print "builds05.version = ",builds05[0]['revision']
 
 
-f = demjson.encode(job_info_00_parent160518)
-print f
-print json_dumps(f)
-print f.count('kind')
-print f.find('actions')
-print "f.find('alionline')",f.find('alionline')
-print f.index("actions")
-
-#print f.keys()
-#print f.values()
-
-
-
-#tmp = server.get_all_jobs(None)
-#print tmp
-#json_dumps(tmp)
-
-server.build_job('test-agency')
-time.sleep(2)
-#pid_build = os.getpid()
-#get_pid_status(pid_build)
-#print "os.getpid(server.build_job('test-agency')) = ",pid_build
-#print "server.get_running_builds() = ",server.get_running_builds()
 builds = server.get_running_builds()
 builds = demjson.encode(builds)
 builds = builds.find("test-agency")
@@ -73,27 +80,6 @@ print "server.get_running_builds() type = ",builds
 print "server.get_running_builds().count(test-agency) = ",type(server.get_running_builds().count('test-agency)'))
 print "builds type = ",type("builds")
 print "builds = ",builds
-
-#builds = demjson.encode(builds)
-#builds = json.load(builds)
-
-#print "build as demjson.encode: ",build
-#print "keys: ",builds.keys()
-#print "keys: " ,builds.keys()
-#print "values: ",builds.values
-#print "info: " ,builds.find("name")
-'''
-
-
-#builds = json.loads(builds)
-#print "json.JSONEncoder(builds):",builds
-
-builds = json_dumps(builds)
-'''
-
-
-#builds = json.dumps(builds)
-#print "json.dumps: ",builds
 
 
 #实现判断job正在运行的逻辑，若运行，则给出提示，若一结束，则提示结束并退出
@@ -119,25 +105,3 @@ while (builds > 0):
         print "builds.find('test-agency') = ",builds
         break
     continue
-    
-
-'''   
-
-for i in range(1,100):
-    builds = server.get_running_builds()
-    print builds
-    json_dumps(builds)
-    time.sleep(3)
-  
-
-
-queue_info = server.get_queue_info()
-json_dumps(queue_info)
-
-job_info_00_parent160518 = server.get_job_info('00_parent160518', 1)
-print job_info_00_parent160518
-json_dumps(job_info_00_parent160518)
-'''
-#demoDictList is the value we want format to output
-#jsonDumpsIndentStr = json.dumps(tmp,indent=1,encoding='utf-8')
-#print "jsonDumpsIndentStr=",jsonDumpsIndentStr
