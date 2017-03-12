79b37907dbdf494cec11dee212140fddf1ed47a3
liuyanchao <ycliu912@qq.com>
2017-03-12 21:32:25 +0800

    init

---
+++ py-redis/redis-base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+"""
+@version: python2.7
+@author: 'liuyanchao'
+@contract: ycliu1283@gmail.com
+@file: redis-base1.PY
+@time: 17/3/12 下午9:28
+"""
+
+print "hello world"
+
